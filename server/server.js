a720dbce24252bf5ec28fd666038f10050ec08a9
Anton <mrservice.ip@gmail.com>
2018-07-10 13:05:56 +0700

    add books routes

--- server/server.js
+++ server/server.js
@@ -20,6 +20,67 @@ mongoose.connect(config.DATABASE);
 //config port from server
 const port = process.env.PORT || 3001;
 
+//get
+app.get('/api/getBook', (req, res) => {
+  let id = req.query.id;
+
+  Book.findById(id, (err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.send(doc);
+  })
+})
+
+app.get('/api/books', (req, res) => {
+  let skip = parseInt(req.query.skip);
+  let limit = parseInt(req.query.limit);
+  let order = req.query.order;
+
+  Book.find().skip(skip).sort({_id: order}).limit(limit).exec((err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.send(doc);
+  })
+})
+
+//post
+app.post('/api/book', (req, res) => {
+  const book = new Book(req.body);
+
+  book.save((err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.status(200).json({
+      post: true,
+      bookId: doc._id
+    });
+  })
+})
+
+//update
+app.post('/api/book_update', (req, res) => {
+
+  Book.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.status(200).json({
+      success: true,
+      doc
+    });
+  })
+})
+
+//delete
+app.delete('/api/delete_book', (req, res) => {
+  let id = req.query.id;
+
+  Book.findByIdAndRemove(id, (err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.json(true);
+  })
+})
+
 app.listen(port, () => {
   console.log(`server start port: ${port}`)
 })
